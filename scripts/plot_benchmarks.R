6135bdf30f64aad99ab5bc4ce1d3ccc1cdfe2ef9
Adam Orr <adamjorr@gmail.com>
2020-09-18 13:03:21 -0700

    start adding ways to plot the combined fnrfpr plots

--- scripts/plot_benchmarks.R
+++ scripts/plot_benchmarks.R
@@ -1,39 +1,72 @@
 #!/usr/bin/env Rscript
 library(tidyverse)
 
+# --- Functions to get file paths ---
+
+#return the data directory appended to path
 get_data_dir <- function(path){
   return(paste0("../data/", path))
 }
 
+#return file name from stems where subpath
+#is a dir in the data directory and suffix
+#is the part after the stem.
+#[../data/subpath/stem1suffix, ../data/subpath/stem2suffix, ...]
 get_file <- function(stem, subpath, suffix){
-  #return file name from stems where subpath
-  #is a dir in the data directory and suffix
-  #is the part after the stem.
   dir <- paste0(subpath, paste0('/', stem))
   return(paste0(get_data_dir(dir), suffix))
 }
 
-get_benchmark_file <- function(stem, subpath = '.'){
-  return(get_file(stem, subpath, ".benchmark"))
-}
-
+#return ../data/subpath/stem.recal.after.csv,
+# the default is ../data/stem.recal.after.csv
 get_gatk_csv_file <- function(stem, subpath = '.'){
   return(get_file(stem, subpath, ".recal.after.csv"))
 }
 
+# --- Utility Functions ---
+
+#turn a probability to a phred-scaled quality score
 p_to_q <- function(p){
   return(floor(-10*log10(p)))
 }
 
-#returns a df with covariatevalue and reportedquality columns
-average_over_rgs_fnr <- function(df){
+# --- Dataframe Manipulation ---
+
+#Gets only QualityScore and After rows in the dataframe
+#and changes CovariateValue title to PredictedQuality
+get_quality_rows <- function(df){
   df %>%
     filter(CovariateName == 'QualityScore' & Recalibration == 'After') %>%
-    rename(PredictedQuality = CovariateValue) %>%
+    rename(PredictedQuality = CovariateValue)
+}
+
+#Returns a df with covariatevalue and reportedquality columns
+average_over_rgs_fnr <- function(df){
+  df %>%
+    get_quality_rows() %>%
     mutate_at(vars(FalseNegativeRate, PredictedQuality), as.numeric) %>%
     group_by(FalseNegativeRate, PredictedQuality) %>%
     summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
 }
+
+average_over_rgs_fpr <- function(df){
+  df %>%
+    get_quality_rows() %>%
+    mutate_at(vars(FalsePositiveRate, PredictedQuality), as.numeric) %>%
+    group_by(FalsePositiveRate, PredictedQuality) %>%
+    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
+}
+
+average_over_rgs_fnrfpr <- function(df){
+  df %>%
+    get_quality_rows() %>%
+    mutate_at(vars(FalsePositiveRate, FalseNegativeRate, PredictedQuality),
+              as.numeric) %>%
+    group_by(FalseNegativeRate, FalsePositiveRate, PredictedQuality) %>%
+    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
+}
+
+# --- Importing CSVs ---
 
 import_fnr_csvs <- function(fnr){
   csv_files <- get_gatk_csv_file(fnr)
@@ -42,6 +75,26 @@ import_fnr_csvs <- function(fnr){
   df <- bind_rows(dfs, .id = 'FalseNegativeRate')
   return(df)
 }
+
+import_fpr_csvs <- function(fpr){
+  csv_files <- get_gatk_csv_file(fpr, subpath = 'fpr')
+  dfs <- map(csv_files, read_csv)
+  names(dfs) <- fpr
+  df <- bind_rows(dfs, .id = 'FalsePositiveRate')
+  return(df)
+}
+
+import_fnrfpr_csvs <- function(fnr, fpr){
+  stems <- unlist(map(fnr, paste0, '_', fpr))
+  csv_files <- get_gatk_csv_file(stems, subpath = 'fnrfpr')
+  dfs <- map(csv_files, read_csv)
+  names(dfs) <- stems
+  df <- bind_rows(dfs, .id = 'FNR_FPR') %>%
+    separate(FNR_FPR, into = c("FalseNegativeRate","FalsePositiveRate"),
+             sep = "_", convert = TRUE)
+}
+
+# --- Plotting Data ---
 
 plot_fnr_csvs <- function(gatk_df){
   ggplot(gatk_df, aes(PredictedQuality, ActualQuality)) +
@@ -55,44 +108,6 @@ plot_fnr_csvs <- function(gatk_df){
     coord_fixed(ratio = 1) +
     theme_minimal(base_size = 18) + 
     theme(plot.margin = margin(0,0,0,0))
-}
-
-import_fnr_benchmarks <- function(fnr){
-  benchmark_files <- get_benchmark_file(fnr)
-  dfs <- map(benchmark_files, read_table2, col_names = c('PredictedQuality','ActualQuality','FalseNegativeRate','NumBases'))
-  df <- bind_rows(dfs)
-  return(df)
-}
-
-plot_comparison_dfs <- function(comparison_dfs){
-  ggplot(comparison_dfs, aes(PredictedQuality, ActualQuality)) +
-    geom_abline(slope = 1, intercept = 0) +
-    geom_point(aes(color = CalibrationMethod), size = 2) +
-    geom_line(aes(color = CalibrationMethod), size = 1) +
-    scale_color_brewer('Calibration\nMethod', palette = 'Dark2') +
-    scale_x_continuous("Predicted Quality") +
-    scale_y_continuous('Actual Quality') +
-    ggtitle('GATK and KBBQ Perform Similarly') +
-    coord_fixed(ratio = 1) +
-    theme_minimal(base_size = 18) +
-    theme(plot.margin = margin(0,0,0,0))
-}
-
-import_fpr_csvs <- function(fpr){
-  csv_files <- get_gatk_csv_file(fpr, subpath = 'fpr')
-  dfs <- map(csv_files, read_csv)
-  names(dfs) <- fpr
-  df <- bind_rows(dfs, .id = 'FalsePositiveRate')
-  return(df)
-}
-
-average_over_rgs_fpr <- function(df){
-  df %>%
-    filter(CovariateName == 'QualityScore' & Recalibration == 'After') %>%
-    rename(PredictedQuality = CovariateValue) %>%
-    mutate_at(vars(FalsePositiveRate, PredictedQuality), as.numeric) %>%
-    group_by(FalsePositiveRate, PredictedQuality) %>%
-    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
 }
 
 plot_fpr_csvs <- function(gatk_df){
@@ -109,17 +124,33 @@ plot_fpr_csvs <- function(gatk_df){
     theme(plot.margin = margin(0,0,0,0))
 }
 
-fnr <- c(0, 20, 40, 60, 80, 100)
-kbbq_benchmark_files <- get_benchmark_file(fnr)
-gatk_csv_files <- get_gatk_csv_file(fnr)
+plot_comparison_dfs <- function(comparison_dfs){
+  ggplot(comparison_dfs, aes(PredictedQuality, ActualQuality)) +
+    geom_abline(slope = 1, intercept = 0) +
+    geom_point(aes(color = CalibrationMethod), size = 2) +
+    geom_line(aes(color = CalibrationMethod), size = 1) +
+    scale_color_brewer('Calibration\nMethod', palette = 'Dark2') +
+    scale_x_continuous("Predicted Quality") +
+    scale_y_continuous('Actual Quality') +
+    ggtitle('GATK and KBBQ Perform Similarly') +
+    coord_fixed(ratio = 1) +
+    theme_minimal(base_size = 18) +
+    theme(plot.margin = margin(0,0,0,0))
+}
 
-gatk_df <- average_over_rgs(import_fnr_csvs(fnr))
+
+
+# --- Main --- 
+
+fnr <- c(0, 20, 40, 60, 80, 100)
+fpr <- c(0, 20, 40, 60, 80, 100)
+
+fnr_df <- average_over_rgs(import_fnr_csvs(fnr))
 
 pdf('../figures/fnr.pdf', width = 9, height = 7)
-plot_fnr_csvs(gatk_df)
+plot_fnr_csvs(fnr_df)
 dev.off()
 
-# kbbq_df <- import_fnr_benchmarks(fnr)
 kbbq_comparison_df <- get_gatk_csv_file('kbbq') %>%
   read_csv() %>%
   filter(CovariateName == 'QualityScore' & Recalibration == 'After')
@@ -141,14 +172,13 @@ pdf('../figures/comparison.pdf', width = 9, height = 7)
 plot_comparison_dfs(comparison_df)
 dev.off()
 
-fpr <- c(0,20,40,60,80,100)
-df <- average_over_rgs_fpr(import_fpr_csvs(fpr))
+fpr_df <- average_over_rgs_fpr(import_fpr_csvs(fpr))
 
 pdf('../figures/fpr.pdf', width = 9, height = 7)
-plot_fpr_csvs(df)
+plot_fpr_csvs(fpr_df)
 dev.off()
     
-
+fnrfpr_df <- average_over_rgs_fnrfpr(import)
 
 
 
