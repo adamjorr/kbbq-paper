0431f1fd4662f7b6fee0fff7c1cee14a6984b132
Adam Orr <adamjorr@gmail.com>
2020-09-20 01:10:44 -0700

    add rmse and plots for fnrfpr

--- scripts/plot_benchmarks.R
+++ scripts/plot_benchmarks.R
@@ -26,8 +26,8 @@ get_gatk_csv_file <- function(stem, subpath = '.'){
 # --- Utility Functions ---
 
 #turn a probability to a phred-scaled quality score
-p_to_q <- function(p){
-  return(floor(-10*log10(p)))
+p_to_q <- function(p, maxscore = 43){
+  return(if_else(p != 0, floor(-10*log10(p)), maxscore))
 }
 
 # --- Dataframe Manipulation ---
@@ -40,13 +40,21 @@ get_quality_rows <- function(df){
     rename(PredictedQuality = CovariateValue)
 }
 
+#Calculate an RMSE column, which is the square root of the average squared
+# difference between actual and predicted quality
+get_RMSE <- function(df){
+  df %>%
+    mutate(RMSE = sqrt(mean((ActualQuality - PredictedQuality)^2)))
+}
+
 #Returns a df with covariatevalue and reportedquality columns
 average_over_rgs_fnr <- function(df){
   df %>%
     get_quality_rows() %>%
     mutate_at(vars(FalseNegativeRate, PredictedQuality), as.numeric) %>%
     group_by(FalseNegativeRate, PredictedQuality) %>%
-    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
+    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations))) %>%
+    get_RMSE()
 }
 
 average_over_rgs_fpr <- function(df){
@@ -54,7 +62,8 @@ average_over_rgs_fpr <- function(df){
     get_quality_rows() %>%
     mutate_at(vars(FalsePositiveRate, PredictedQuality), as.numeric) %>%
     group_by(FalsePositiveRate, PredictedQuality) %>%
-    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
+    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations))) %>%
+    get_RMSE()
 }
 
 average_over_rgs_fnrfpr <- function(df){
@@ -63,7 +72,8 @@ average_over_rgs_fnrfpr <- function(df){
     mutate_at(vars(FalsePositiveRate, FalseNegativeRate, PredictedQuality),
               as.numeric) %>%
     group_by(FalseNegativeRate, FalsePositiveRate, PredictedQuality) %>%
-    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations)))
+    summarize(ActualQuality = p_to_q(sum(Errors)/sum(Observations))) %>%
+    get_RMSE()
 }
 
 # --- Importing CSVs ---
@@ -138,6 +148,47 @@ plot_comparison_dfs <- function(comparison_dfs){
     theme(plot.margin = margin(0,0,0,0))
 }
 
+abbreviate_rate <- function(str){
+  str_replace_all(str,
+                  c("FalsePositiveRate" = "FPR", "FalseNegativeRate" = "FNR"))
+}
+
+rate_labeller <- labeller(
+  .rows = function(str){str_replace(str,"^100$","FNR: 100")},
+  .cols = function(str){str_replace(str,"^0$","FPR: 0")}
+)
+
+plot_fnrfpr_csvs <- function(gatk_df){
+  #remove the largest 5 values; these are outliers
+
+  scale_lim <- gatk_df$RMSE %>%
+    sort() %>% unique() %>% .[1:(length(.)-5)] %>% range()
+  scale_vals <- function(x, ...){
+    y <- scales::rescale(x, to = c(0,1), from = scale_lim)
+    y[y>1] <- 1
+    y
+  }
+  
+  ggplot(gatk_df, aes(PredictedQuality, ActualQuality)) +
+    geom_abline(slope = 1, intercept = 0) +
+    geom_point(aes(color = RMSE), size = 1) +
+    geom_line(aes(color = RMSE), size = 1) +
+    # geom_point(aes(color = factor(FalseNegativeRate)), size = 2) +
+    # geom_line(aes(color = factor(FalseNegativeRate)), size = 1) +
+    # for colors we should look into making each color the same as the heat
+    # map that we ultimately make for RMSE.
+    facet_grid(rows = vars(FalseNegativeRate), cols = vars(FalsePositiveRate),
+               as.table = FALSE, labeller = rate_labeller) +
+    #continuous version of scale_color_brewer
+    scale_color_viridis_c('RMSE', option = "viridis", rescaler = scale_vals,
+                          limits = scale_lim, oob = scales::squish) + 
+    scale_x_continuous("Predicted Quality") +
+    scale_y_continuous("Actual Quality") +
+    ggtitle('False Negative Rate Has Higher Impact On RMSE\nThan False Positive Rate') +
+    coord_fixed(ratio = 1) +
+    theme_minimal(base_size = 18) + 
+    theme(plot.margin = margin(0,0,0,0))
+}
 
 
 # --- Main --- 
@@ -178,7 +229,7 @@ pdf('../figures/fpr.pdf', width = 9, height = 7)
 plot_fpr_csvs(fpr_df)
 dev.off()
     
-fnrfpr_df <- average_over_rgs_fnrfpr(import)
-
+fnrfpr_df <- average_over_rgs_fnrfpr(import_fnrfpr_csvs(fnr, fpr))
+plot_fnrfpr_csvs(fnrfpr_df)
 
 
